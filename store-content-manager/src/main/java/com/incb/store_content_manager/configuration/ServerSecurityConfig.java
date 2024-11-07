4bec60524aa1b213da3be33f1c1fdbd96a179419
Tunir <vincenthavoc@gmail.com>
2024-11-07 11:42:02 +0530

    add: OAuth 2.0 v1

--- store-content-manager/src/main/java/com/incb/store_content_manager/configuration/ServerSecurityConfig.java
+++ store-content-manager/src/main/java/com/incb/store_content_manager/configuration/ServerSecurityConfig.java
@@ -2,6 +2,7 @@ package com.incb.store_content_manager.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,10 +14,27 @@ public class ServerSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        /*
         return httpSecurity
                 .authorizeHttpRequests((authenticator) -> authenticator.anyRequest().permitAll())
                 .csrf(AbstractHttpConfigurer::disable)
+                .oauth2ResourceServer(authServer -> authServer.jwt(Customizer.withDefaults()))
                 .build();
+        */
+
+        return httpSecurity
+                .authorizeHttpRequests(
+                        authenticator -> authenticator
+                        .requestMatchers("/")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .oauth2Login(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+
     }
 
 }
